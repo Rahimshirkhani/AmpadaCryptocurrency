594ef7d48cdd05a899bf99a0325d9dfcd4a6b0d1
Rahim Shirkhani <Rahim_shirkhani@yahoo.com>
2022-09-18 23:49:20 +0430

    Refactor Sidebar,Add Topbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,37 @@
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './menu/sideBar/side-bar/side-bar.component';
+import { SideBarComponent } from './Menu/sideBar/side-bar.component';
 
 
-import {AccordionModule} from 'primeng/accordion';
-import {MenuItem} from 'primeng/api';
+import { SlideMenuModule } from 'primeng/slidemenu';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TopBarComponent } from './menu/top-bar/top-bar.component';
+import { IndexComponent } from './index/index.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent
+    SideBarComponent,
+    TopBarComponent,
+    IndexComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    SlideMenuModule,
+    ButtonModule,
     AppRoutingModule
   ],
   providers: [],
