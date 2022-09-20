e20fc1f6f1208b3211d727c32cf43abee3ca41be
shirkhani.r <shirkhani.r@CL-107>
2022-09-20 16:39:56 +0430

    Add Charts

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {MessageService} from 'primeng/api';
+import {Subscription} from 'rxjs';
+import {AppConfig} from "../domain/app-config";
+import {AppConfigService} from "../service/app-config-service";
+
 
 @Component({
   selector: 'app-index',
@@ -9,10 +13,62 @@ import {MessageService} from 'primeng/api';
 export class IndexComponent implements OnInit {
   value1: any = 70;
 
-  constructor() { }
+  chartdata: any;
+  chartOptions: any;
 
-  ngOnInit(): void {
-    }
+  basicData: any;
+  basicOptions: any;
 
+  constructor() {
   }
+
+  ngOnInit() {
+
+    this.chartOptions = {
+      responsive: true,
+      maintainAspectRatio: false
+    };
+    this.chartdata = {
+      labels: ['BTC', 'ADA', 'SHIB', 'CRO', 'ETH', 'OTHER'],
+      datasets: [
+        {
+          data: [250, 300, 350, 200, 250, 50],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }
+      ]
+    };
+
+    this.basicOptions = {
+      responsive: true,
+    };
+    this.basicData = {
+      labels: ['Today', 'This Week', 'This Month', 'This Year', '...'],
+      datasets: [
+        {
+          label: '',
+          data: [10, 20, 30, 20, 40, 10],
+          fill: false,
+          borderColor: '#453DE0 ',
+          tension: .4
+        }
+      ]
+    };
+  }
+
+}
 
